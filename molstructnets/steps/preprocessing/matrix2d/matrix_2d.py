4f0de2105772555062780618fc73bfdb13b6708f
patrick-winter-knime <patrick.winter@uni-konstanz.de>
2017-10-23 16:29:27 +0200

    Added random transformation to 2d matrix.

--- molstructnets/steps/preprocessing/matrix2d/matrix_2d.py
+++ molstructnets/steps/preprocessing/matrix2d/matrix_2d.py
@@ -1,5 +1,5 @@
 from util import data_validation, misc, file_structure, file_util, logger, progressbar, concurrent_max, concurrent_set,\
-    thread_pool, constants, hdf5_util, concurrent_min
+    thread_pool, constants, hdf5_util, concurrent_min, reference_data_set
 from steps.preprocessing.matrix2d import bond_positions
 import h5py
 from rdkit import Chem
@@ -27,18 +27,20 @@ class Matrix2D:
         parameters = list()
         parameters.append({'id': 'scale', 'name': 'Scale factor (default: 2)', 'type': float, 'default': 2.0,
                            'description': 'The scaling factor used to change the size of the resulting grid.'})
-        parameters.append({'id': 'width', 'name': 'Width (default: automatic)', 'type': int,
-                           'default': None,
+        parameters.append({'id': 'width', 'name': 'Width (default: automatic)', 'type': int, 'default': None,
                            'description': 'The maximum width of molecules that will fit into the preprocessed data'
                                           ' array.'})
-        parameters.append({'id': 'height', 'name': 'Height (default: automatic)', 'type': int,
-                           'default': None,
+        parameters.append({'id': 'height', 'name': 'Height (default: automatic)', 'type': int, 'default': None,
                            'description': 'The maximum height of molecules that will fit into the preprocessed data'
                                           ' array.'})
-        parameters.append({'id': 'symbols', 'name': 'Force symbols (default: none)', 'type': str,
-                           'default': None, 'description': 'Symbols in the given string will be added to the index'
-                                                           ' in addition to symbols found in the data set. Multiple'
-                                                           ' symbols are separated by a semicolon (e.g. \'F;Cl;S\').'})
+        parameters.append({'id': 'symbols', 'name': 'Force symbols (default: none)', 'type': str, 'default': None,
+                           'description': 'Symbols in the given string will be added to the index in addition to'
+                                          ' symbols found in the data set. Multiple symbols are separated by a'
+                                          ' semicolon (e.g. \'F;Cl;S\').'})
+        parameters.append({'id': 'transformations', 'name': 'Number of transformations for each matrix (default: none)',
+                           'type': int, 'default': 0,
+                           'description': 'The number of transformations done for each matrix in the training data'
+                                          ' set.'})
         return parameters
 
     @staticmethod
@@ -98,18 +100,19 @@ class Matrix2D:
             max_symbol_length = 0
             for symbol in symbols:
                 max_symbol_length = max(max_symbol_length, len(symbol))
-            index = hdf5_util.create_dataset(preprocessed_h5, 'index', (len(symbols),), dtype='S' + str(max_symbol_length))
+            index = hdf5_util.create_dataset(preprocessed_h5, 'index', (len(symbols),),
+                                             dtype='S' + str(max_symbol_length))
             index_lookup = {}
             for i in range(len(symbols)):
                 index_lookup[symbols[i]] = i
                 index[i] = symbols[i].encode('utf-8')
             global_parameters[constants.GlobalParameters.input_dimensions] = (max_x + 1, max_y + 1, len(index))
             preprocessed = hdf5_util.create_dataset(preprocessed_h5, file_structure.Preprocessed.preprocessed,
-                                                          (len(smiles_data), max_x + 1, max_y + 1, len(index)),
-                                                          dtype='I', chunks=(1, max_x + 1, max_y + 1, len(index)))
+                                                    (len(smiles_data), max_x + 1, max_y + 1, len(index)), dtype='I',
+                                                    chunks=(1, max_x + 1, max_y + 1, len(index)))
             atom_locations = hdf5_util.create_dataset(preprocessed_h5, 'atom_locations',
-                                                          (len(smiles_data), max_nr_atoms, 2),
-                                                          dtype='I', chunks=(1, max_nr_atoms, 2))
+                                                      (len(smiles_data), max_nr_atoms, 2), dtype='I',
+                                                      chunks=(1, max_nr_atoms, 2))
             logger.log('Writing matrices')
             with progressbar.ProgressBar(len(smiles_data)) as progress:
                 with thread_pool.ThreadPool(number_threads) as pool:
@@ -118,6 +121,33 @@ class Matrix2D:
                                     smiles_data[chunk['start']:chunk['end'] + 1], index_lookup, min_x, min_y,
                                     chunk['start'], local_parameters['scale'], progress)
                     pool.wait()
+            if local_parameters['transformations'] > 0:
+                logger.log('Writing transformed training data')
+                partition_h5 = h5py.File(global_parameters[constants.GlobalParameters.partition_data], 'r')
+                train = partition_h5[file_structure.Partitions.train]
+                preprocessed_training = \
+                    hdf5_util.create_dataset(preprocessed_h5, file_structure.Preprocessed.preprocessed_training,
+                                             (train.shape[0] * local_parameters['transformations'],
+                                              preprocessed.shape[1], preprocessed.shape[2], preprocessed.shape[3]),
+                                             dtype='I', chunks=(1, preprocessed.shape[1], preprocessed.shape[2],
+                                                                preprocessed.shape[3]))
+                preprocessed_training_ref = \
+                    hdf5_util.create_dataset(preprocessed_h5,
+                                             file_structure.Preprocessed.preprocessed_training_references,
+                                             (preprocessed_training.shape[0],), dtype='I')
+                train_smiles_data = reference_data_set.ReferenceDataSet(train, smiles_data)
+                chunks = misc.chunk(len(train), number_threads)
+                originals_set = concurrent_set.ConcurrentSet()
+                with progressbar.ProgressBar(len(preprocessed_training)) as progress:
+                    with thread_pool.ThreadPool(number_threads) as pool:
+                        for chunk in chunks:
+                            pool.submit(Matrix2D.write_transformed_2d_matrices, preprocessed_training,
+                                        preprocessed_training_ref, train_smiles_data[chunk['start']:chunk['end'] + 1],
+                                        index_lookup, min_x, min_y, chunk['start'], local_parameters['scale'],
+                                        originals_set, local_parameters['transformations'], len(train_smiles_data),
+                                        global_parameters[constants.GlobalParameters.seed] + chunk['start'], train,
+                                        progress)
+                        pool.wait()
             data_h5.close()
             preprocessed_h5.close()
             file_util.move_file(temp_preprocessed_path, preprocessed_path)
@@ -141,47 +171,113 @@ class Matrix2D:
             progress.increment()
 
     @staticmethod
-    def write_2d_matrices(preprocessed, atom_locations, smiles_data, index_lookup, min_x, min_y, offset, scale_factor, progress):
+    def write_2d_matrices(preprocessed, atom_locations, smiles_data, index_lookup, min_x, min_y, offset, scale_factor,
+                          progress):
         for i in range(len(smiles_data)):
-            preprocessed_row = numpy.zeros((preprocessed.shape[1], preprocessed.shape[2], preprocessed.shape[3]), dtype='int16')
-            atom_locations_row = numpy.zeros((atom_locations.shape[1], atom_locations.shape[2]), dtype='int16')
             smiles = smiles_data[i].decode('utf-8')
             molecule = Chem.MolFromSmiles(smiles)
             AllChem.Compute2DCoords(molecule)
-            atom_positions = dict()
-            for atom in molecule.GetAtoms():
-                symbol_index = index_lookup[atom.GetSymbol()]
-                position = molecule.GetConformer().GetAtomPosition(atom.GetIdx())
-                x = round(position.x * scale_factor) - min_x
-                y = round(position.y * scale_factor) - min_y
-                preprocessed_row[x, y, symbol_index] = 1
-                atom_locations_row[atom.GetIdx(), 0] = x
-                atom_locations_row[atom.GetIdx(), 1] = y
-                atom_positions[atom.GetIdx()] = [x, y]
-            bond_positions_ = bond_positions.calculate(molecule, atom_positions)
-            for bond in molecule.GetBonds():
-                bond_symbol = get_bond_symbol(bond.GetBondType())
-                if bond_symbol is not None:
-                    bond_symbol_index = index_lookup[bond_symbol]
-                    for position in bond_positions_[bond.GetIdx()]:
-                        preprocessed_row[position[0], position[1], bond_symbol_index] = 1
+            preprocessed_row, atom_locations_row = Matrix2D.create_2d_matrix(molecule, index_lookup, scale_factor,
+                                                                             min_x, min_y, preprocessed.shape,
+                                                                             atom_locations_shape=atom_locations.shape)
             preprocessed[i + offset, :] = preprocessed_row[:]
             atom_locations[i + offset, :] = atom_locations_row[:]
             progress.increment()
 
+    @staticmethod
+    def write_transformed_2d_matrices(preprocessed_training, preprocessed_training_ref, smiles_data, index_lookup,
+                                      min_x, min_y, offset, scale_factor, originals_set, number_transformations,
+                                      offset_per_transformation, seed, train_ref, progress):
+        random_ = numpy.random.RandomState(seed)
+        for i in range(len(smiles_data)):
+            original_index = train_ref[i + offset]
+            smiles = smiles_data[i].decode('utf-8')
+            molecule = Chem.MolFromSmiles(smiles)
+            number_atoms = len(molecule.GetAtoms())
+            number_distances = int((number_atoms * number_atoms - number_atoms) / 2)
+            start = 0
+            if originals_set.add(smiles):
+                AllChem.Compute2DCoords(molecule)
+                preprocessed_row = Matrix2D.create_2d_matrix(molecule, index_lookup, scale_factor, min_x, min_y,
+                                                             preprocessed_training.shape)[0]
+                preprocessed_training[i + offset, :] = preprocessed_row[:]
+                preprocessed_training_ref[i + offset] = original_index
+                start += 1
+                progress.increment()
+            failed_attempts = 0
+            for j in range(start, number_transformations):
+                invalid = True
+                while invalid:
+                    if failed_attempts < 10:
+                        distances = random_.rand(number_distances)
+                        AllChem.Compute2DCoordsMimicDistmat(molecule, distances)
+                    else:
+                        AllChem.Compute2DCoords(molecule)
+                    invalid = not Matrix2D.fits(molecule, min_x, min_y, preprocessed_training.shape[1] - 1,
+                                                preprocessed_training.shape[2] - 1, scale_factor)
+                    if invalid:
+                        failed_attempts += 1
+                index = i + offset + offset_per_transformation * j
+                preprocessed_row = Matrix2D.create_2d_matrix(molecule, index_lookup, scale_factor, min_x, min_y,
+                                                             preprocessed_training.shape)[0]
+                preprocessed_training[index, :] = preprocessed_row[:]
+                preprocessed_training_ref[index] = original_index
+                progress.increment()
 
-def get_bond_symbol(bond_type):
-    if bond_type == BondType.ZERO:
-        return None
-    elif bond_type == BondType.SINGLE:
-        return '-'
-    elif bond_type == BondType.DOUBLE:
-        return '='
-    elif bond_type == BondType.TRIPLE:
-        return '#'
-    elif bond_type == BondType.QUADRUPLE:
-        return '$'
-    elif bond_type == BondType.AROMATIC:
-        return ':'
-    else:
-        return '-'
+    @staticmethod
+    def fits(molecule, min_x, min_y, max_x, max_y, scale_factor):
+        invalid = False
+        for atom in molecule.GetAtoms():
+            position = molecule.GetConformer().GetAtomPosition(atom.GetIdx())
+            x = round(position.x * scale_factor) - min_x
+            y = round(position.y * scale_factor) - min_y
+            if not (0 <= x <= max_x and 0 <= y <= max_y):
+                invalid = True
+                break
+        return not invalid
+
+    @staticmethod
+    def create_2d_matrix(molecule, index_lookup, scale_factor, min_x, min_y, preprocessed_shape,
+                         atom_locations_shape=None):
+        preprocessed_row = numpy.zeros((preprocessed_shape[1], preprocessed_shape[2], preprocessed_shape[3]),
+                                       dtype='int16')
+        if atom_locations_shape is None:
+            atom_locations_row = None
+        else:
+            atom_locations_row = numpy.zeros((atom_locations_shape[1], atom_locations_shape[2]), dtype='int16')
+        atom_positions = dict()
+        for atom in molecule.GetAtoms():
+            symbol_index = index_lookup[atom.GetSymbol()]
+            position = molecule.GetConformer().GetAtomPosition(atom.GetIdx())
+            x = round(position.x * scale_factor) - min_x
+            y = round(position.y * scale_factor) - min_y
+            preprocessed_row[x, y, symbol_index] = 1
+            if atom_locations_row is not None:
+                atom_locations_row[atom.GetIdx(), 0] = x
+                atom_locations_row[atom.GetIdx(), 1] = y
+            atom_positions[atom.GetIdx()] = [x, y]
+        bond_positions_ = bond_positions.calculate(molecule, atom_positions)
+        for bond in molecule.GetBonds():
+            bond_symbol = Matrix2D.get_bond_symbol(bond.GetBondType())
+            if bond_symbol is not None:
+                bond_symbol_index = index_lookup[bond_symbol]
+                for position in bond_positions_[bond.GetIdx()]:
+                    preprocessed_row[position[0], position[1], bond_symbol_index] = 1
+        return preprocessed_row, atom_locations_row
+
+    @staticmethod
+    def get_bond_symbol(bond_type):
+        if bond_type == BondType.ZERO:
+            return None
+        elif bond_type == BondType.SINGLE:
+            return '-'
+        elif bond_type == BondType.DOUBLE:
+            return '='
+        elif bond_type == BondType.TRIPLE:
+            return '#'
+        elif bond_type == BondType.QUADRUPLE:
+            return '$'
+        elif bond_type == BondType.AROMATIC:
+            return ':'
+        else:
+            return '-'
