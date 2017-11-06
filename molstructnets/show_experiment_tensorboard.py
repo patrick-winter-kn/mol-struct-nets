f326f471b4906f61d64a0222680a15ac7a8b7428
patrick-winter-knime <patrick.winter@uni-konstanz.de>
2017-11-06 12:44:27 +0100

    Added script to easily show the tensorboard for an existing experiment.

--- molstructnets/show_experiment_tensorboard.py
+++ molstructnets/show_experiment_tensorboard.py
@@ -1,41 +1,33 @@
 from util import initialization
-from keras import backend
-import gc
-import time
+import os
+from os import path
+import sys
 import argparse
 from experiments import experiment
-from util import file_structure, logger, file_util, constants
+from util import file_structure, constants, file_util
 from steps import steps_repository
-import h5py
-import re
-import datetime
+from steps.datageneration import data_generation_repository
+from steps.targetgeneration import target_generation_repository
+import threading
+import time
 
 
 def get_arguments():
-    parser = argparse.ArgumentParser(description='Runs an existing experiment')
+    parser = argparse.ArgumentParser(description='Shows the tensorboard of an existing experiment')
     parser.add_argument('experiment', type=str, help='Path to the experiment file')
     parser.add_argument('--data_set', type=str, default=None, help='Data set name')
     parser.add_argument('--target', type=str, default=None, help='Target name')
     parser.add_argument('--partition', type=str, default=None, help='Partition name')
-    parser.add_argument('--step', type=int, default=None, help='Run the experiment up to the given step')
     return parser.parse_args()
 
 
-def get_n(global_parameters_):
-    n_ = None
-    if constants.GlobalParameters.data_set in global_parameters_:
-        data_set_path = file_structure.get_data_set_file(global_parameters_)
-        if file_util.file_exists(data_set_path):
-            data_h5 = h5py.File(data_set_path)
-            if file_structure.DataSet.smiles in data_h5.keys():
-                n_ = len(data_h5[file_structure.DataSet.smiles])
-            data_h5.close()
-    return n_
+def open_tensorboard():
+    time.sleep(2)
+    os.system('xdg-open http://localhost:6006')
 
 
 args = get_arguments()
-start_time = datetime.datetime.now()
-logger.log('Starting experiment at ' + str(start_time))
+executable_path = sys.executable[:sys.executable.rfind(path.sep) + 1]
 experiment_ = experiment.Experiment(args.experiment)
 global_parameters = dict()
 global_parameters[constants.GlobalParameters.seed] = initialization.seed
@@ -47,18 +39,11 @@ if args.target is not None:
     global_parameters[constants.GlobalParameters.target] = file_structure.find_target(global_parameters, args.target)
 if args.partition is not None:
     global_parameters[constants.GlobalParameters.partition_data] = file_structure.find_partition(global_parameters, args.partition)
-n = get_n(global_parameters)
-if n is not None:
-    global_parameters[constants.GlobalParameters.n] = n
 nr_steps = experiment_.number_steps()
-if args.step is not None:
-    nr_steps = args.step + 1
 for i in range(nr_steps):
     step_config = experiment_.get_step(i)
-    type_name = steps_repository.instance.get_step_name(step_config['type'])
+    type_id = step_config['type']
     step = steps_repository.instance.get_step_implementation(step_config['type'], step_config['id'])
-    logger.log('=' * 100)
-    logger.log('Starting step: ' + type_name + ': ' + step.get_name())
     parameters = {}
     implementation_parameters = step.get_parameters()
     for parameter in implementation_parameters:
@@ -67,13 +52,11 @@ for i in range(nr_steps):
     if 'parameters' in step_config:
         for parameter in step_config['parameters']:
             parameters[parameter] = step_config['parameters'][parameter]
-    step.check_prerequisites(global_parameters, parameters)
-    step.execute(global_parameters, parameters)
-    logger.log('Finished step: ' + type_name + ': ' + step.get_name())
-    backend.clear_session()
-logger.log('=' * 100)
-end_time = datetime.datetime.now()
-logger.log('Finished execution of experiment successfully at ' + str(end_time))
-logger.log('Duration of experiment: ' + str(end_time - start_time))
-gc.collect()
-time.sleep(1)
+    if type_id == data_generation_repository.instance.get_id():
+        global_parameters[constants.GlobalParameters.data_set] = file_util.get_filename(step.get_result_file(global_parameters, parameters), False)
+    if type_id == target_generation_repository.instance.get_id():
+        global_parameters[constants.GlobalParameters.target] = file_util.get_filename(step.get_result_file(global_parameters, parameters), False)
+tensorboard_path = file_structure.get_network_file(global_parameters)
+tensorboard_path = tensorboard_path[:tensorboard_path.rfind('.')] + '-tensorboard'
+threading.Thread(target=open_tensorboard).start()
+os.system(executable_path + 'tensorboard --logdir=' + tensorboard_path)
--- molstructnets/util/file_structure.py
+++ molstructnets/util/file_structure.py
@@ -1,4 +1,5 @@
 from util import file_util, constants
+import re
 
 
 class DataSet:
@@ -107,9 +108,44 @@ def get_interpretation_folder(global_parameters):
                                      global_parameters[constants.GlobalParameters.data_set],
                                      global_parameters[constants.GlobalParameters.target], 'interpretation')
 
+
 def get_attentionmap_file(global_parameters):
     return file_util.resolve_subpath(global_parameters[constants.GlobalParameters.root], 'experiments',
                                      global_parameters[constants.GlobalParameters.experiment],
                                      global_parameters[constants.GlobalParameters.data_set],
                                      global_parameters[constants.GlobalParameters.target], 'interpretation',
                                      'attention_map.h5')
+
+
+def find_file(global_parameters_, folder_path, name):
+    prefix = file_util.resolve_subpath(folder_path, name)
+    if file_util.file_exists(prefix + '.h5'):
+        return name
+    files = file_util.list_files(folder_path)
+    pattern = re.compile(re.escape(prefix) + '.*')
+    for file in files.copy():
+        if file_util.is_folder(file):
+            files.remove(file)
+        if not pattern.match(file):
+            files.remove(file)
+    if len(files) == 0:
+        raise IOError('No file with prefix ' + name + ' found in ' + folder_path)
+    elif len(files) > 1:
+        raise IOError('Multiple files with prefix ' + name + ' found in ' + folder_path)
+    return file_util.get_filename(files[0], False)
+
+
+def find_data_set(global_parameters_, name):
+    data_set_folder = get_data_set_folder(global_parameters_)
+    return find_file(global_parameters_, data_set_folder, name)
+
+
+def find_target(global_parameters_, name):
+    target_folder = get_target_folder(global_parameters_)
+    return find_file(global_parameters_, target_folder, name)
+
+
+def find_partition(global_parameters_, name):
+    partition_folder = get_partition_folder(global_parameters_)
+    name = find_file(global_parameters_, partition_folder, name)
+    return file_util.resolve_subpath(partition_folder, name + '.h5')
