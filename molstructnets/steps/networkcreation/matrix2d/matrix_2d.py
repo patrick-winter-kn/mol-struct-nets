bf4b11b5f2e7073eb6eddab4b69960c06d400c40
patrick-winter-knime <patrick.winter@uni-konstanz.de>
2017-11-24 15:50:08 +0100

    Removed dropout from 2D matrix network.

--- molstructnets/steps/networkcreation/matrix2d/matrix_2d.py
+++ molstructnets/steps/networkcreation/matrix2d/matrix_2d.py
@@ -1,7 +1,7 @@
 from util import file_structure, file_util, logger, constants
 from keras.models import Model
 from keras.layers import Input
-from keras.layers.core import Dense, Flatten, Dropout
+from keras.layers.core import Dense, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras import initializers
 
@@ -36,48 +36,41 @@ class Matrix2D:
             initializer = initializers.he_uniform()
             input_layer = Input(shape=global_parameters[constants.GlobalParameters.input_dimensions], name='input')
             layer = input_layer
-            layer = Dropout(0.3, name='dropout_input')(layer)
 
             # Block 1
             layer = Convolution2D(16, 3, activation='relu', padding='same', name='convolution_1_1', kernel_initializer=initializer)(layer)
             layer = Convolution2D(16, 3, activation='relu', padding='same', name='convolution_1_2', kernel_initializer=initializer)(layer)
             layer = Convolution2D(16, 3, activation='relu', padding='same', name='convolution_1_3', kernel_initializer=initializer)(layer)
             layer = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool_1')(layer)
-            layer = Dropout(0.75, name='dropout_convolution_1')(layer)
 
             # Block 2
             layer = Convolution2D(32, 3, activation='relu', padding='same', name='convolution_2_1', kernel_initializer=initializer)(layer)
             layer = Convolution2D(32, 3, activation='relu', padding='same', name='convolution_2_2', kernel_initializer=initializer)(layer)
             layer = Convolution2D(32, 3, activation='relu', padding='same', name='convolution_2_3', kernel_initializer=initializer)(layer)
             layer = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool_2')(layer)
-            layer = Dropout(0.75, name='dropout_convolution_2')(layer)
 
             # Block 3
             layer = Convolution2D(64, 3, activation='relu', padding='same', name='convolution_3_1', kernel_initializer=initializer)(layer)
             layer = Convolution2D(64, 3, activation='relu', padding='same', name='convolution_3_2', kernel_initializer=initializer)(layer)
             layer = Convolution2D(64, 3, activation='relu', padding='same', name='convolution_3_3', kernel_initializer=initializer)(layer)
             layer = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool_3')(layer)
-            layer = Dropout(0.75, name='dropout_convolution_3')(layer)
 
             # Block 4
             layer = Convolution2D(128, 3, activation='relu', padding='same', name='convolution_4_1', kernel_initializer=initializer)(layer)
             layer = Convolution2D(128, 3, activation='relu', padding='same', name='convolution_4_2', kernel_initializer=initializer)(layer)
             layer = Convolution2D(128, 3, activation='relu', padding='same', name='convolution_4_3', kernel_initializer=initializer)(layer)
             layer = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool_4')(layer)
-            layer = Dropout(0.75, name='dropout_convolution_4')(layer)
 
             # Block 5
             layer = Convolution2D(256, 3, activation='relu', padding='same', name='convolution_5_1', kernel_initializer=initializer)(layer)
             layer = Convolution2D(256, 3, activation='relu', padding='same', name='convolution_5_2', kernel_initializer=initializer)(layer)
             layer = Convolution2D(256, 3, activation='relu', padding='same', name='convolution_5_3', kernel_initializer=initializer)(layer)
             layer = MaxPooling2D((2, 2), strides=(2, 2), name='max_pool_5')(layer)
-            layer = Dropout(0.75, name='dropout_convolution_5')(layer)
 
             layer = Flatten(name='flatten_1')(layer)
             layer = Dense(32, activation='relu', name='dense_1', kernel_initializer=initializer)(layer)
-            layer = Dropout(0.75, name='dropout_dense_1')(layer)
             output_layer = Dense(2, activation='softmax', name='output', kernel_initializer=initializer)(layer)
             model = Model(inputs=input_layer, outputs=output_layer)
-            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['binary_accuracy'])
             file_util.make_folders(network_path)
             model.save(network_path)
