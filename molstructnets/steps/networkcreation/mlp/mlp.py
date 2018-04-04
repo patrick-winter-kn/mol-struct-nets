263b0cd6aa9178f7633ae7c83b93990c103bafd2
Patrick Winter <patrick.winter@uni-konstanz.de>
2018-04-04 17:12:07 +0200

    Added simple MLP to networks.

---
+++ molstructnets/steps/networkcreation/mlp/mlp.py
@@ -0,0 +1,45 @@
+from util import file_structure, file_util, logger, constants
+from keras.models import Model
+from keras.layers import Input
+from keras.layers.core import Dense, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras import initializers, optimizers
+
+
+class MLP:
+
+    @staticmethod
+    def get_id():
+        return 'mlp'
+
+    @staticmethod
+    def get_name():
+        return 'Multi Layer Perceptron'
+
+    @staticmethod
+    def get_parameters():
+        parameters = list()
+        return parameters
+
+    @staticmethod
+    def check_prerequisites(global_parameters, local_parameters):
+        pass
+
+    @staticmethod
+    def execute(global_parameters, local_parameters):
+        network_path = file_structure.get_network_file(global_parameters)
+        if file_util.file_exists(network_path):
+            logger.log('Skipping step: ' + network_path + ' already exists')
+        else:
+            initializer = initializers.he_uniform()
+            input_layer = Input(shape=global_parameters[constants.GlobalParameters.input_dimensions], name='input')
+            layer = input_layer
+            layer = Dropout(0.3, name='input_dropout')(layer)
+            layer = Flatten(name='flatten_1')(layer)
+            layer = Dense(128, activation='relu', name='dense', kernel_initializer=initializer)(layer)
+            output_layer = Dense(2, activation='softmax', name='output', kernel_initializer=initializer)(layer)
+            model = Model(inputs=input_layer, outputs=output_layer)
+            optimizer = optimizers.Adam(lr=0.0001)
+            model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['binary_accuracy'])
+            file_util.make_folders(network_path)
+            model.save(network_path)
--- molstructnets/steps/networkcreation/network_creation_repository.py
+++ molstructnets/steps/networkcreation/network_creation_repository.py
@@ -4,6 +4,7 @@ from steps.networkcreation.tensor2d import tensor_2d
 from steps.networkcreation.image import image
 from steps.networkcreation.vgg19 import vgg19
 from steps.networkcreation.customtensor2d import custom_tensor_2d
+from steps.networkcreation.mlp import mlp
 
 
 class DataGenerationRepository(repository.Repository):
@@ -18,6 +19,7 @@ class DataGenerationRepository(repository.Repository):
 
 
 instance = DataGenerationRepository()
+instance.add_implementation(mlp.MLP)
 instance.add_implementation(tensor_smiles.TensorSmiles)
 instance.add_implementation(tensor_2d.Tensor2D)
 instance.add_implementation(custom_tensor_2d.CustomTensor2D)
