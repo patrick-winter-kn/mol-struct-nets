Increased speed of index calculation in partitioning and gauss filter in preprocessing. Preprocessed file does now use int8 for binary data and float 32 otherwise. Bugfix for progressbar in statistics calculation.

molstructnets/util/gauss.py
@@ -1,5 +1,5 @@
 from scipy.ndimage import filters
-from util import hdf5_util, file_util, logger, progressbar
+from util import hdf5_util, file_util, logger, progressbar, misc
 import h5py
 
 
@@ -9,14 +9,23 @@ def apply_gauss(path, data_set_name, sigma):
     hdf5_util.set_property(temp_path, data_set_name + '_gauss_sigma', sigma)
     file_h5 = h5py.File(temp_path, 'r+')
     data_set = file_h5[data_set_name]
-    slices = list()
-    for length in data_set.shape[:-1]:
-        slices.append(slice(0,length))
-    logger.log('Applying gauss')
-    with progressbar.ProgressBar(data_set.shape[-1]) as progress:
-        for i in range(data_set.shape[-1]):
-            index = tuple(slices + [i])
-            data_set[index] = filters.gaussian_filter(data_set[index], sigma=sigma)
+    chunked_data = misc.get_chunked_array(data_set, fraction=0.5)
+    logger.log('Applying gauss (in ' + chunked_data.number_chunks() + ' chunks)')
+    with progressbar.ProgressBar(chunked_data.number_chunks() * 2 + chunked_data.number_chunks()
+                                 * data_set.shape[-1]) as progress:
+        for i in range(chunked_data.number_chunks()):
+            chunked_data.load_chunk(i)
             progress.increment()
+            data = chunked_data[:]
+            slices = list()
+            for length in data.shape[:-1]:
+                slices.append(slice(0,length))
+            for j in range(data.shape[-1]):
+                index = tuple(slices + [j])
+                data[index] = filters.gaussian_filter(data[index], sigma=sigma)
+                progress.increment()
+            chunked_data.write_current_chunk()
+            progress.increment()
+    chunked_data.unload()
     file_h5.close()
     file_util.copy_file(temp_path, path)