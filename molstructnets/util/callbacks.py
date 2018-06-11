6ac23e960b60e4353a0f5eec61c9f3a3b2d081dc
Patrick Winter <patrick.winter@uni-konstanz.de>
2018-06-11 15:53:38 +0200

    Fix in checkpoint callback.

--- molstructnets/util/callbacks.py
+++ molstructnets/util/callbacks.py
@@ -1,5 +1,6 @@
 from keras import callbacks
-from util import hdf5_util
+from util import hdf5_util, file_util
+import time
 
 
 class CustomCheckpoint(callbacks.Callback):
@@ -10,4 +11,6 @@ class CustomCheckpoint(callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         self.model.save(self.file_path, overwrite=True)
+        while not file_util.file_exists(self.file_path):
+            time.sleep(0.001)
         hdf5_util.set_property(self.file_path, 'epochs_trained', epoch + 1)
