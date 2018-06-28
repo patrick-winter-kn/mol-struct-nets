d8f05ffab0ea0dd9481e097c03292f975e7f8f46
Patrick Winter <patrick.winter@uni-konstanz.de>
2018-06-28 17:07:32 +0200

    Fix: Edit experiment could not be started without an experiment file.

--- molstructnets/util/initialization.py
+++ molstructnets/util/initialization.py
@@ -10,11 +10,12 @@ silent_loading = True
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['PYTHONHASHSEED'] = '0'
 seed = random.randint(0, numpy.iinfo(numpy.uint32).max)
-experiment_path = os.path.abspath(sys.argv[1])
-if os.path.exists(experiment_path):
-    dict_ = json.load(open(experiment_path))
-    if 'seed' in dict_:
-        seed = dict_['seed']
+if len(sys.argv) >= 2:
+    experiment_path = os.path.abspath(sys.argv[1])
+    if os.path.exists(experiment_path):
+        dict_ = json.load(open(experiment_path))
+        if 'seed' in dict_:
+            seed = dict_['seed']
 random.seed(seed)
 numpy.random.seed(seed)
 matplotlib.use('Agg')
