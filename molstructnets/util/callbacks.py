99e43a2a4be157aef9f7f8a2adb6beace3caa3bc
Patrick Winter <patrick.winter@uni-konstanz.de>
2018-07-19 13:26:23 +0200

    Added run of garbage collector before saving the model, to try a fix of 'Resource temporarily unavailable' error.

--- molstructnets/util/callbacks.py
+++ molstructnets/util/callbacks.py
@@ -1,4 +1,5 @@
 from keras import callbacks
+import gc
 
 
 class CustomCheckpoint(callbacks.Callback):
@@ -8,6 +9,7 @@ class CustomCheckpoint(callbacks.Callback):
         self.file_path = file_path
 
     def on_epoch_end(self, epoch, logs=None):
+        gc.collect()
         self.model.save(self.file_path, overwrite=True)
         with open(self.file_path[:-3] + '-epochs.txt', 'w') as file:
             file.write(str(epoch + 1))
