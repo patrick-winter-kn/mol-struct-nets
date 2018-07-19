66ae0903900548eaf0496e5c7f2021de8ae72966
Patrick Winter <patrick.winter@uni-konstanz.de>
2018-07-19 15:52:34 +0200

    Different fix attempt for 'Resource temporarily unavailable' error.

--- molstructnets/util/callbacks.py
+++ molstructnets/util/callbacks.py
@@ -1,5 +1,5 @@
 from keras import callbacks
-import gc
+from util import file_util
 
 
 class CustomCheckpoint(callbacks.Callback):
@@ -9,7 +9,8 @@ class CustomCheckpoint(callbacks.Callback):
         self.file_path = file_path
 
     def on_epoch_end(self, epoch, logs=None):
-        gc.collect()
-        self.model.save(self.file_path, overwrite=True)
+        temp_path = file_util.get_temporary_file_path('model')
+        self.model.save(temp_path)
+        file_util.move_file(temp_path, self.file_path)
         with open(self.file_path[:-3] + '-epochs.txt', 'w') as file:
             file.write(str(epoch + 1))
